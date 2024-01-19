680ed82fd8e99d9b87f0b8c4201edaf55ddfb3f9
jowkarm <jokar.mehdi2@gmail.com>
2024-01-18 21:46:46 -0800

    Initial commit

---
+++ src/main/java/edu/greenriver/sdev/jokesdad/JokesDadApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.jokesdad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesDadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesDadApplication.class, args);
+    }
+
+}
